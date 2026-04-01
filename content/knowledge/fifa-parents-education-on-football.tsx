b0b51b387a92887794b17cbd98592cdaa0be2d79
ubyoth <ubyakpan@gmail.com>
2026-04-01 23:17:59 +0100

    Update fifa-parents-education-on-football.tsx

--- content/knowledge/fifa-parents-education-on-football.tsx
+++ content/knowledge/fifa-parents-education-on-football.tsx
@@ -6,7 +6,7 @@ const SITE_URL = "https://apexsportslaw.com";
 const SLUG = "fifa-parents-education-on-football";
 const PAGE_URL = `${SITE_URL}/knowledge/${SLUG}`;
 
-const OG_IMAGE = "https://res.cloudinary.com/dq7vjblwd/image/upload/v1775081179/FIFA_parents_education_oppq6h.png";
+const OG_IMAGE = "https://res.cloudinary.com/dq7vjblwd/image/upload/w_1200,h_630,c_fill/v1775081179/FIFA_parents_education_oppq6h.png";
 const OG_IMAGE_FALLBACK = `${SITE_URL}/og/default.jpg`;
 
 const TITLE =
