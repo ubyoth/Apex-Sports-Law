8fb2d07e066670e6f6a12df976379ae1f3284185
ubyoth <ubyakpan@gmail.com>
2026-08-19 12:18:25 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import prerender from 'vite-plugin-prerender';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
@@ -13,6 +14,18 @@ export default defineConfig(({ mode }) => {
     plugins: [
       react(),
       tailwindcss(),
+      prerender({
+        staticDir: path.join(__dirname, 'dist'),
+        routes: [
+          '/',
+          '/knowledge',
+          '/knowledge/termination-football-contract-outstanding-salaries-fifa-article-14bis',
+        ],
+        renderer: '@prerenderer/renderer-puppeteer',
+        rendererOptions: {
+          renderAfterTime: 3000,
+        }
+      })
     ],
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
